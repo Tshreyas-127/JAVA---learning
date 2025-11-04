2aa4d70dd9ecaebcc404e30a3a0727de165b9441
Tshreyas-127 <shreyastukrul127@gmail.com>
2025-11-04 09:56:45 +0530

    Add files via upload

---
+++ Day1/Assignmet_Day1/src/Pattern/pattern3.java
@@ -0,0 +1,14 @@
+package pattern3;
+
+public class pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<3;i++){
+			for(int j=0;j<=i;j++) 
+				System.out.print("*");	
+		
+		System.out.println();
+		}
+	}
+}
